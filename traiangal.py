9d098df2780addd83cfab2e2642a1028e97826be
Saeideh-Moghaddam <108235776+saeideh-moghaddam@users.noreply.github.com>
2022-06-29 22:15:55 +0430

    Add files via upload

---
+++ traiangal.py
@@ -0,0 +1,16 @@
+import math
+a = int(input("please type first rid1: "))
+b = int(input("please type second rid2: "))
+c = int(input("please type third rid3: "))
+
+if a<b+c and a<b+c and a<b+c :
+    print("right")
+else:
+    print("incorrect")
+a = int(input("please type first rid1: "))
+b = int(input("please type second rid2: "))
+c = int(input("please type third rid3: "))
+if a<b+c and a<b+c and a<b+c :
+    print('You can create a Triangle')
+else:
+    print('You can\'t create a Triangle')  
